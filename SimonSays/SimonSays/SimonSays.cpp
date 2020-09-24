c089584bfe326846345a7f9fa87ff424d4529445
Dylan Smit <dylan.smit01@gmail.com>
2020-09-24 14:48:03 +0200

    Fixed bugs and added more function

--- SimonSays/SimonSays/SimonSays.cpp
+++ SimonSays/SimonSays/SimonSays.cpp
@@ -14,35 +14,44 @@ int countDigit(long long n){
     return count;
 }
 
-void playSimonSays(long long simonNumber) {
+void playSimonSays(long long simonNumber, bool isHardMode) {
     int answer;
-
-    simonNumber = simonNumber * 10 + rand() % 9 + 1;
+    int add;
+    srand((unsigned int)time(NULL));
+    add = rand() % 9 + 1;
+    simonNumber = simonNumber * 10 + add;
 
     system("CLS");
-    cout << "Simon says: " << simonNumber << endl;
+    if (isHardMode == true) { cout << "Simon says: " << add << endl; }
+    else { cout << "Simon says: " << simonNumber << endl; }
+    
     this_thread::sleep_for(chrono::milliseconds(2000));
     system("CLS");
-    cout << "What did simon say?" << endl;
+    if (isHardMode == true) { cout << "What did simon say in total?" << endl; }
+    else { cout << "What did simon say?" << endl; }
+
     cin >> answer;
 
     if (answer != simonNumber) {
-        if (simonNumber < 100) cout << "YOU SUCK!";
-        else cout << "You died, your score is  " << countDigit(simonNumber);
+        if (simonNumber < 100) { cout << "YOU SUCK!"; }
+        else { cout << "You died, your score is  " << countDigit(simonNumber); }
     }
-    else playSimonSays(simonNumber);
+    else playSimonSays(simonNumber, isHardMode);
 }
 
 int main()
 {
     long long simonSays = 0;
     char question;
+    char diffuculty;
+    bool isHardMode;
 
 
     cout << "Start game? (Y/N)" << endl;
     cin >> question;
 
-    if (!question) {
+    if (question == 'n' || question == 'N') {
+        system("CLS");
         cout << "Smell ya later";
         this_thread::sleep_for(chrono::milliseconds(2000));
         system("CLS");
@@ -50,7 +59,13 @@ int main()
         return 0;
     }
 
-    playSimonSays(simonSays);
+    cout << "Hard mode? (Y/N)" << endl;
+    cin >> diffuculty;
+
+    if (diffuculty == 'y' || diffuculty == 'Y') { isHardMode = true; }
+    else { isHardMode = false; }
+
+    playSimonSays(simonSays, isHardMode);
 
     return 0;
 }
